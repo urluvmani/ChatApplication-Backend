d5d7b86bc9d09b471ae7c80131f06dcc87fbfa9f
Hafiz Muhammad Abdul Rehman <abdulrehman90190@gmail.com>
2025-11-19 13:15:30 -0800

    domain added

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000'],
+        origin:['https://mani-chats.vercel.app'],
         methods:['GET', 'POST'],
     },
 });
