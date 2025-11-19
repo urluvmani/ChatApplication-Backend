b1f095ffe3f62baecdfd6956e1e1a24d80748bdd
Hafiz Muhammad Abdul Rehman <abdulrehman90190@gmail.com>
2025-11-19 13:34:59 -0800

    domain added

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-// const express = require('express')// method-1
-import express from "express"; // method-2
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app,server } from "./socket/socket.js";
-dotenv.config({});
+import { app, server } from "./socket/socket.js";
 
- 
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
-// middleware
-app.use(express.urlencoded({extended:true}));
-app.use(express.json()); 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOption={
-    origin: ['https://mani-chats.vercel.app', /\.vercel\.app$/],
-    origin: true, 
-credentials: true
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
-};
-app.use(cors(corsOption)); 
+// Routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
 
-
-// routes
-app.use("/api/v1/user",userRoute); 
-app.use("/api/v1/message",messageRoute);
- 
-
-server.listen(PORT, ()=>{
-    connectDB();
-    console.log(`Server listen at prot ${PORT}`);
+// Start Server
+server.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running on port ${PORT}`);
 });
-
--- socket/socket.js
+++ socket/socket.js
@@ -1,15 +1,15 @@
-import {Server} from "socket.io";
-import http from "http";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
-
 const server = http.createServer(app);
+
 const io = new Server(server, {
-    cors:{
-        origin:['https://mani-chats.vercel.app'],
-        methods:['GET', 'POST'],
-    },
+  cors: {
+    origin: true,
+    credentials: true
+  }
 });
 
 export const getReceiverSocketId = (receiverId) => {
